dba2bb5afd74070712e3d7a8e184febcf0ef35f8
Claudia Torres <alet24665@gmail.com>
2023-12-19 08:47:16 -0300

    agregamos mensje por consola, documento html y ventana emergente

---
+++ js/01-variables.js
@@ -0,0 +1,10 @@
+// mostrar comentarios desde el archivo de js
+console.log("Mensaje por consola");
+
+// mostrar un mensaje en el archivo html
+document.write(
+  '<p class="text-center text-primary">Primer mensaje en el documento HTML</p>'
+);
+
+// ventanas emergentes
+alert('Ejemplo de una alerta')
