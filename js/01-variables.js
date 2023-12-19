bb7daa4e7abd3fd26a17d29395312b14136088ec
Claudia Torres <alet24665@gmail.com>
2023-12-19 09:25:39 -0300

    agregamos variables creadas con let y con const

--- js/01-variables.js
+++ js/01-variables.js
@@ -7,4 +7,22 @@ document.write(
 );
 
 // ventanas emergentes
-alert('Ejemplo de una alerta')
+alert("Ejemplo de una alerta");
+
+// declarar e  inicializar una variable
+// inicializar o asignar un valor se usa =
+// crear o declarar una variable (var - let - const)
+// var ya no se usa
+
+let regaloNavidad = "Iphone 15 Pro max";
+
+// concatenar: unir el texto con el texto de una variable se usa +
+document.write("Regalo de navidad 2023: " + regaloNavidad + "<br>");
+
+// modificar el contenido de una variable creada con la palabra let
+regaloNavidad = "Rtx 4090 Rog Strix";
+document.write("Regalo de navidad 2023: " + regaloNavidad);
+
+const urlUniverse = "https://universe.rollingcodeschool.com/";
+
+document.write("<br>Pagina de Universe: " + urlUniverse);
