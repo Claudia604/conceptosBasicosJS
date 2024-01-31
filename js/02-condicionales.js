0fda259adae0bb46438d25c6bdaca765aa126dbd
Claudia Torres <alet24665@gmail.com>
2024-01-31 12:01:18 -0300

    agregamos nuevos alert al ejercicio de condicionales

--- js/02-condicionales.js
+++ js/02-condicionales.js
@@ -3,11 +3,9 @@ const edad = parseInt(prompt("Ingrese su edad"));
 // if(condicion logica){todo el codigo que quiero ejecutar si se cumple la condicion}
 if (edad >= 18 && edad <= 71) {
   alert("Puede tener un permiso para conducir");
+} else if (edad < 18) {
+  alert("Sos menor de edad, no puedes tener un permiso para conducir");
 } else {
-  if (edad < 18) {
-    alert("Sos menor de edad, no puedes tener un permiso para conducir");
-  } else {
-    alert("No puede manejar");
-  }
+  alert("No puede manejar");
 }
 // else(lo que quiero ejecutar si no se cumple la condicion logica)
